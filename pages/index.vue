519fb1cd2781cd2b63f77d2a55984e150f1bce86
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 <sharapov.savely@yandex.ru>
2023-04-19 23:17:49 +0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="index-container">
+    <div class="index-preview" ref="indexPreview">
+      <div>
+        <img :src="require('@/static/f11.svg')"/>
+        <span>Fullscreen mode</span>
+      </div>
+
+      <div class="start-button" @click="redirectToMain">Let`s go</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+        disclaimerIsOver : false
+    }
+  },
+  methods: {
+    async redirectToMain() {
+      await this.$router.push('/main')
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.indexPreview.style.opacity = '1'
+    }, 500)
+
+  },
+}
+</script>
---
+++ pages/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index-container">
+    <img :src="require('@/static/background.png')"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main"
+}
+</script>
+
+<style scoped>
+
+</style>
