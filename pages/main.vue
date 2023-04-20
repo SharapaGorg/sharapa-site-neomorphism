98f7667ed361410561e5b80353315f4c9531fb99
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 <sharapov.savely@yandex.ru>
2023-04-20 20:27:05 +0300

    changed background

--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="index-container">
     <img :src="require('@/static/background.png')"/>
+<!--    <img :src="require('@/static/arrow.svg')" class="back-arrow" @click="backToIndex"/>-->
+
+    <div class="gpt-input-container"></div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "main"
+  name: "main",
+  methods: {
+    async backToIndex() {
+      await this.$router.push('/')
+    }
+  }
 }
 </script>
 
